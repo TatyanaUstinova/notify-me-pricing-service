3dec151a2857e361692ad6d125e19b0d4849f2e9
Tatyana Ustinova <ustinova.tatyana.s@mail.ru>
2017-06-04 23:24:52 +0400

    Updated app.py

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from src.models.stores.views import store_blueprint
 
 
 app = Flask(__name__)
-app.config.from_object("config")
+app.config.from_object("src.config")
 app.secret_key = "super - super key"
 
 
